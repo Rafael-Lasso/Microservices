d2d1e3c4537a9883f25b79e1a65b09867196e335
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-21 22:16:40 -0300

    router products created

--- product/index.go
+++ product/index.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func loadData() []byte {
+
 	jsonFile, err := os.Open("products.json")
+	
 	if err != nil {
 		fmt.Println("erro: ",err.Error())
 	}
@@ -17,6 +22,17 @@ func loadData() []byte {
 	return data
 }
 
+func ListProducts(w http.ResponseWriter, r *http.Request) {
+
+	products := loadData()
+	w.Write([]byte(products))
+}
+
 func main() {
-  fmt.Println(string(loadData()))
+
+	r := mux.NewRouter()
+	r.HandleFunc("/products", ListProducts)
+	http.ListenAndServe(":8081", r)
+
+	fmt.Println(string(loadData()))
 }
