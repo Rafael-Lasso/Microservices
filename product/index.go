4a943cce7a8a9685431f160a59c41ace472a8c8b
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-21 22:45:48 -0300

    Add function to get product by Id

--- product/index.go
+++ product/index.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
-
 	"github.com/gorilla/mux"
 )
+
+
+
+type Product struct {
+	Uuid string		`json:"uuid"`
+	Product string 	`json:"product"`
+	Price float64 	`json:"price,string"`
+}
+
+type Products struct{
+	Products []Product
+}
 
 func loadData() []byte {
 
@@ -28,10 +40,26 @@ func ListProducts(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(products))
 }
 
+func GetProductById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := loadData()
+
+	var products Products
+	json.Unmarshal(data, &products)
+
+	for _, v := range products.Products {
+		if v.Uuid == vars["id"] {
+			product, _ := json.Marshal(v)
+			w.Write([]byte(product))
+		}
+	}
+}
+
 func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/products", ListProducts)
+	r.HandleFunc("/product/{id}", GetProductById)
 	http.ListenAndServe(":8081", r)
 
 	fmt.Println(string(loadData()))
