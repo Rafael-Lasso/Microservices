3733da90c10fef58027f9b5a12e4493e4701540f
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-21 22:01:05 -0300

    Read json file

---
+++ product/index.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func loadData() []byte {
+	jsonFile, err := os.Open("products.json")
+	if err != nil {
+		fmt.Println("erro: ",err.Error())
+	}
+	defer jsonFile.Close()
+
+	data, err := ioutil.ReadAll(jsonFile)
+	return data
+}
+
+func main() {
+  fmt.Println(string(loadData()))
+}
